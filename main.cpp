8efd2c2f4cae2abf869b51e4ab3bd1ca9b16c0bc
trapperdot00 <126814871+trapperdot00@users.noreply.github.com>
2023-04-21 00:00:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,58 +12,10 @@ using std::end;
 using std::begin;
 using std::tolower;
 
-
-void printMap(const char *arr, const unsigned &playerPosIndex, const bool &playerShow, const unsigned w, const unsigned h)
-{
-    system("CLS");
-    for (size_t index = 0; index != w*h; ++index) {
-        if (!(index % w) && (index > 0))
-            cout << endl;
-            if (playerPosIndex != index || !playerShow) {
-                cout << arr[index] << ' ';
-            }
-            else {
-                cout << "X ";
-            }
-
-    }
-    cout << '\n' << endl;
-}
-
-void initMap(char *arr, unsigned &playerPosIndex, const unsigned w, const unsigned h)
-{
-    bool oddScale = (w % 2 && h % 2);
-    for (size_t index = 0; index != w*h; ++index) {
-        arr[index] = '-';
-        if ((oddScale && index == w*h/2) || (!oddScale && index == (w*h+w)/2)) {
-            playerPosIndex = index;
-        }
-    }
-}
-
-void moveTo(unsigned &playerPosIndex, const unsigned w, const unsigned h, const char direction)
-{
-    switch (tolower(direction))
-    {
-        case 'u':   // up
-            playerPosIndex = playerPosIndex - w < w*h ? playerPosIndex - w : playerPosIndex;
-            break;
-        case 'd':   // down
-            playerPosIndex = playerPosIndex + w < w*h ? playerPosIndex + w : playerPosIndex;
-            break;
-        case 'l':   // left
-            playerPosIndex = (playerPosIndex - 1) % w < playerPosIndex % w ? playerPosIndex - 1 : playerPosIndex;
-            break;
-        case 'r':   // right
-            playerPosIndex = (playerPosIndex + 1) % w > playerPosIndex % w ? playerPosIndex + 1 : playerPosIndex;
-            break;
-    }
-}
-
-void placeObj1(char *arr, const unsigned playerPosIndex)
-{
-    arr[playerPosIndex] = arr[playerPosIndex] != 'O' ? 'O' : '-';
-}
+void printMap(const char*, const unsigned&, const bool&, const unsigned&, const unsigned&);
+void initMap(char*, unsigned&, const unsigned&, const unsigned&);
+void moveTo(unsigned&, const unsigned&, const unsigned&, const char&);
+void placeObj1(char *arr, const unsigned&);
 
 int main()
 {
@@ -82,36 +34,29 @@ int main()
             if (GetKeyState('W') & 0x8000 || GetKeyState(VK_UP) & 0x8000) {
                 direction = 'u';
                 moveTo(playerIndex, width, height, direction);
-                //printMap(gameMap, playerIndex, playerShow, width, height);
+                printMap(gameMap, playerIndex, playerShow, width, height);
             }
             else if (GetKeyState('S') & 0x8000 || GetKeyState(VK_DOWN) & 0x8000) {
                 direction = 'd';
                 moveTo( playerIndex, width, height, direction);
-                //printMap(gameMap, playerIndex, playerShow, width, height);
+                printMap(gameMap, playerIndex, playerShow, width, height);
             }
             else if (GetKeyState('A') & 0x8000 || GetKeyState(VK_LEFT) & 0x8000) {
                 direction = 'l';
                 moveTo(playerIndex, width, height, direction);
-                //printMap(gameMap, playerIndex, playerShow, width, height);
+                printMap(gameMap, playerIndex, playerShow, width, height);
             }
             else if (GetKeyState('D') & 0x8000 || GetKeyState(VK_RIGHT) & 0x8000) {
                 direction = 'r';
                 moveTo(playerIndex, width, height, direction);
-                //printMap(gameMap, playerIndex, playerShow, width, height);
-
+                printMap(gameMap, playerIndex, playerShow, width, height);
             }
             else if (GetKeyState('E') & 0x8000 || GetKeyState(VK_SPACE) & 0x8000) {
                 placeObj1(gameMap, playerIndex);
-                //printMap(gameMap, playerIndex, playerShow, width, height);
+                printMap(gameMap, playerIndex, playerShow, width, height);
             }
             else if (GetKeyState('H') & 0x8000) {
                 playerShow = !playerShow;
-                //printMap(gameMap, playerIndex, playerShow, width, height);
-            }
-            if ((GetKeyState(VK_TAB) & 0x0001)!=0) {
-                //printMap(gameMap, playerIndex, playerShow, width, height);
-            }
-            else if (GetKeyState('R') & 0x8000) {
                 printMap(gameMap, playerIndex, playerShow, width, height);
             }
             sleep_for(100ms);
@@ -119,3 +64,55 @@ int main()
     }
     return 0;
 }
+
+void printMap(const char *arr, const unsigned &playerPosIndex, const bool &playerShow, const unsigned &w, const unsigned &h)
+{
+    system("CLS");
+    for (size_t index = 0; index != w*h; ++index) {
+        if (!(index % w) && (index > 0))
+            cout << endl;
+            if (playerPosIndex != index || !playerShow) {
+                cout << arr[index] << ' ';
+            }
+            else {
+                cout << "X ";
+            }
+    }
+    cout << '\n' << endl;
+}
+
+void initMap(char *arr, unsigned &playerPosIndex, const unsigned &w, const unsigned &h)
+{
+    bool oddScale = (w % 2 && h % 2);
+    for (size_t index = 0; index != w*h; ++index) {
+        arr[index] = '-';
+        if ((oddScale && index == w*h/2) || (!oddScale && index == (w*h+w)/2)) {
+            playerPosIndex = index;
+        }
+    }
+}
+
+void moveTo(unsigned &playerPosIndex, const unsigned &w, const unsigned &h, const char &direction)
+{
+    switch (tolower(direction))
+    {
+        case 'u':   // up
+            playerPosIndex = playerPosIndex - w < w*h ? playerPosIndex - w : playerPosIndex;
+            break;
+        case 'd':   // down
+            playerPosIndex = playerPosIndex + w < w*h ? playerPosIndex + w : playerPosIndex;
+            break;
+        case 'l':   // left
+            playerPosIndex = (playerPosIndex - 1) % w < playerPosIndex % w ? playerPosIndex - 1 : playerPosIndex;
+            break;
+        case 'r':   // right
+            playerPosIndex = (playerPosIndex + 1) % w > playerPosIndex % w ? playerPosIndex + 1 : playerPosIndex;
+            break;
+    }
+}
+
+void placeObj1(char *arr, const unsigned &playerPosIndex)
+{
+    arr[playerPosIndex] = arr[playerPosIndex] != 'O' ? 'O' : '-';
+}
+
